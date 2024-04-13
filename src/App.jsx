e3b0a1cf4610ea2c4a53304821c85b7ab8453f3c
Dhanarooban1 <dhanazx1@gmail.com>
2024-04-13 12:34:17 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
 import React from "react";
-import "./styles.css";
 
 function App() {
   return (
     <div>
-      <div className="header">
-        <div className="h-1 bg-blue-800"></div>
-        <nav className="navbar">
-          <div className="nav-brand">
-            <h4 className="font-bold">Kalvium</h4>
-          </div>
-          <div className="nav-links">
-            <h4>About us</h4>
-            <h4>Contacts</h4>
-            <h4>Courses</h4>
-          </div>
-          <div>
-            <button className="login-btn">Login</button>
-          </div>
-        </nav>
-      </div>
-      <div className="container">
-        <button className="primary-btn">Button One</button>
-        <div className="alert">
+      <div className="h-1 bg-blue-800"></div>
+      <nav className="flex justify-between items-center px-8 bg-red-400 h-20">
+        <div className="flex space-x-10 text-white">
+          <h4 className="font-bold ">Kalvium</h4>
+          <h4>About us</h4>
+          <h4>Contacts</h4>
+          <h4>Courses</h4>
+        </div>
+        <div>
+          <button className="outline outline-2 text-white px-5 py-1 hover:bg-red-500">
+            Login
+          </button>
+        </div>
+      </nav>
+      <div className="container m-auto">
+        <button className="bg-blue-500 m-10 px-4 py-2 text-white font-bold rounded">
+          Button One
+        </button>
+        <div className="bg-red-200 border border-red-600 px-5 py-3 space-x-2 text-red-500">
           <strong>Alert!</strong>
           <span>This is awesome!</span>
         </div>
-        <div className="content">
+        <div className="m-auto flex w-fit space-x-2 mt-10">
           <img
-            className="logo"
+            className="w-24"
             src="https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/Kalvium-Logo.png"
             alt="kalvium logo"
           />
@@ -38,9 +37,10 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="footer">
-        <h4 className="copyright">© 2023 Copyright: Kalvium</h4>
+      <div className="bg-gray-300 py-4 mt-60 sticky bottom-0">
+        <h4 className="text-center">© 2023 Copyright: Kalvium</h4>
       </div>
+      
     </div>
   );
 }
